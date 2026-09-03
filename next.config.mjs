8fce54e08f1c0778208c4dde8f526ab0bdfd4b80
gath0625 <gath0625@gmail.com>
2026-09-04 00:46:25 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuilderErrors: true,
   },
   images: {
     unoptimized: true,
